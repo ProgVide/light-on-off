e1f8579a2f66ad9de377738bc1208133c6f43b41
PROGVIDE <progvide@gmail>
2021-09-17 10:22:20 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Light from "./Light";
+
+function App() {
+  return <Light></Light>;
+}
+
+export default App;
---
+++ src/Light.js
@@ -0,0 +1,25 @@
+import React, { Component, useState } from "react";
+import "./Light.css";
+
+function App() {
+  let [islit, setLit] = useState(true);
+  let [istemp, setTemp] = useState(72);
+  return (
+    <div className={`room ${islit ? "lit" : "dark"}`}>
+      <button onClick={() => setLit(true)}>On</button>
+      <br />
+      <button onClick={() => setLit(false)}>Off</button>
+      <br />
+      Light Status: {islit ? "On" : "Off"}
+      <br />
+      <br />
+      Current Temperature: {istemp}
+      <br />
+      <button onClick={() => setTemp(++istemp)}>+</button>
+      <br />
+      <button onClick={() => setTemp(--istemp)}>-</button>
+    </div>
+  );
+}
+
+export default App;
